3f96772da965b620bd1a86033a56ad6411e72c59
Anton Taraev <anti191@gmail.com>
2012-06-16 19:57:52 +0400

    Add state machine

--- app/models/car/violation.rb
+++ app/models/car/violation.rb
@@ -3,6 +3,37 @@ class Car::Violation < ActiveRecord::Base
   has_one :city
   has_attached_file :image
 
+  state_machine :initial => :queried do
+
+    state :queried
+    state :new
+    state :sent
+    state :sent_by_client
+    state :solved
+    state :closed
+
+    event :completion do
+      transition :queried => :new
+    end
+
+    event :send do
+      transition :new => :sent
+    end
+
+    event :send_by_client do
+      transition :new => :send_by_client
+    end
+
+    event :solve do
+      transition :sent => :solved, :sent_by_client => :solved
+    end
+
+    event :close do
+      transition :new => :closed
+    end
+
+  end
+
   def image_url
     image.url
   end
