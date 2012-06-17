8fbd4a6a5f66e613306d04da520286919dd582a8
Anton Taraev <anti191@gmail.com>
2012-06-17 07:33:43 +0400

    Fix map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,17 @@ class HomeController < ApplicationController
 
   def map
     @violations = Car::Violation.with_state(:new).limit(9).all
+    @my_geo_objects = []
+    @violations.each do |violation|
+      geo_object = {
+        geometry: {type: "Point", coordinates: [violation.lat, violation.long]},
+        properties: {
+        clusterCaption: violation.car.try(:number) || "Нет номера",
+        balloonContentBody: violation.description
+        }
+      }
+      @my_geo_objects << geo_object
+    end
+    @my_geo_objects = @my_geo_objects.to_json
   end
 end
